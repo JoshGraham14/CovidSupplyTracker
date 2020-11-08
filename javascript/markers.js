251ffa6d502d03d4ae9a8ef79205a1365028e4bf
Witts-A <71847614+Witts-A@users.noreply.github.com>
2020-11-07 20:50:25 -0500

    added popup + changed text color

--- javascript/markers.js
+++ javascript/markers.js
@@ -6,9 +6,18 @@
 
 */
 
-function addMarker(lng, lat){
-    var marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
+function addMarker(lng, lat, title, contents){
+    // if there is only a title passed to the function, gives a default response
+    if (contents === undefined){
+        contents = "No information available";
+    };
+
+    // creates a new pop-up to be linked with marker
+    var pop = new mapboxgl.Popup({offset: 25, maxWidth: 300}).setHTML("<h1>"+title+"</h1>"+contents);
+    // creates marker and adds it to the map
+    var marker = new mapboxgl.Marker().setPopup(pop).setLngLat([lng, lat]).addTo(map);
+    return marker;
 };
 
-// Queens university marker
-addMarker(-76.4951, 44.2253);
+// Creates a default marker at Queen's campus
+var queensu = addMarker(-76.4951, 44.2253, "Queen's University", "Founded god knows when");
